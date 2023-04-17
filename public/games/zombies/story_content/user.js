d0c1047eaaf0c3d9183402edc2631c553b609e78
Sasha Goloshchapov <ferrata@hey.com>
2023-04-17 17:47:53 -0500

    feat: zombies game, update 2

--- public/games/zombies/story_content/user.js
+++ public/games/zombies/story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5tOVhwDjk57":
+      case "6HNbThZ1u1u":
         Script1();
         break;
   }
@@ -11,21 +11,30 @@ function ExecuteScript(strId)
 function Script1()
 {
   const zeroPad = (num, places) => String(num).padStart(places, '0');
+const player = GetPlayer();
 
-setInterval(function() {
-	const player = GetPlayer();
-	const elapsed = player.GetVar('game_time_elapsed_ms');
-    player.SetVar('game_time_elapsed_ms', elapsed + 125);
+// console.log('1. game_timer_set', player.GetVar('game_timer_set'));
 
-    const limit = player.GetVar('game_time_limit_ms');
-    const timeLeft = limit - elapsed;
-    
-    const milliseconds = parseInt(timeLeft%1000);
-    const seconds = parseInt((timeLeft/1000)%60);
-    const minutes = parseInt((timeLeft/(1000*60))%60);
-    
-    player.SetVar('game_time_elapsed', `${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)}.${zeroPad(milliseconds, 3)}`);
-}, 125);
+if (!player.GetVar('game_timer_set')) {
+	player.SetVar('game_timer_set', new Date().getTime().toString());
+	
+	// console.log('2. calling setInterval');
+
+	setInterval(function() {
+		const started = parseInt(player.GetVar('game_timer_set'));
+		const elapsed = new Date().getTime() - started;
+	    player.SetVar('game_time_elapsed_ms', elapsed);
+	
+	    const limit = player.GetVar('game_time_limit_ms');
+	    const timeLeft = limit - elapsed;
+	    
+	    const milliseconds = parseInt(timeLeft%1000);
+	    const seconds = parseInt((timeLeft/1000)%60);
+	    const minutes = parseInt((timeLeft/(1000*60))%60);
+	    
+	    player.SetVar('game_time_elapsed', `${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)}.${zeroPad(milliseconds, 3)}`);
+	}, 150);
+}
 
 
 

6214c360675e701a2b8f50a010f652f508ed1e36
Sasha Goloshchapov <ferrata@hey.com>
2023-08-25 06:04:18 -0500

    progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
-import App from './app';
+import App from "./app";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(<App />);
